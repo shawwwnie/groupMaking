3ba219bd0918cbfc57443029b3b9d2dde29dd306
JungHwan <qnseh85@gmail.com>
2021-01-14 12:00:09 +0900

    로그인수정

--- groupMaking/src/main/java/com/groupMaking/member/controller/MemberController.java
+++ groupMaking/src/main/java/com/groupMaking/member/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.groupMaking.member.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,9 +22,11 @@ import com.groupMaking.member.service.MemberService;
 import com.groupMaking.member.vo.MemberVO;
 import com.groupMaking.util.LoginCheckException;
 
+
 @Controller
 public class MemberController {
 
+    
 	@Resource
 	private MemberService memberService;
 
@@ -33,7 +39,7 @@ public class MemberController {
 		return new MemberVO();
 	}
 
-	// 회원가입 / SPRING용 / 나중에 이거 주석 풀어서 사용할것
+	// 회원가입 
 
 	
 	@RequestMapping(value="/member/signUp.do", method=RequestMethod.POST) public
@@ -52,6 +58,7 @@ public class MemberController {
 	}
 	 
 
+	//안드로이드용 회원가입
 	@RequestMapping(value = "/member/signUpAndroid.do", method = RequestMethod.POST)
 	public void submitSignUpAndroid(@Valid MemberVO memberVO, BindingResult result, Model model,
 			HttpServletRequest request) {
@@ -72,11 +79,13 @@ public class MemberController {
 	@RequestMapping(value = "/member/login.do", method = RequestMethod.POST)
 	public String submitLogin(@Valid MemberVO memberVO, BindingResult result, HttpSession session,
 			HttpServletResponse response, Model model) {
+		        
 		try {
+			
 			MemberVO member = memberService.login(memberVO.getMem_id());
 
-			// DB에서 가져온 비밀번호가 있는지 여부 체크
-			if (member.getMem_pw() != null) {
+			// DB에서 가져온 비밀번호랑 일치하는지 확인
+			if (member.getMem_pw().equals(memberVO.getMem_pw())) {
 				// 세션에 user로 로그인한 회원의 디테일 저장
 				session.setAttribute("user", member);
 				System.out.println("로그인 성공");
@@ -92,5 +101,40 @@ public class MemberController {
 		}
 
 	}
+	
+	//안드로이드용
+	@RequestMapping(value = "/member/loginAndroid.do", method = RequestMethod.POST)
+	public void submitLoginAndroid(@Valid MemberVO memberVO, BindingResult result, HttpSession session,
+			HttpServletResponse response, Model model) {
+		
+		
+		try {
+			MemberVO member = memberService.login(memberVO.getMem_id());
+			PrintWriter out = response.getWriter();
+			JSONObject obj = new JSONObject();
 
+			System.out.println(member);
+			// DB에서 가져온 비밀번호랑 체크
+			if (member.getMem_pw().equals(memberVO.getMem_pw())) {
+				// 세션에 user로 로그인한 회원의 디테일 저장
+				session.setAttribute("user", member);
+				System.out.println("로그인 성공");
+				
+				 obj.put("result",0); //성공시 0
+				
+			} else {
+				 obj.put("result",1);  //실패시 1
+				throw new LoginCheckException();
+				
+			}
+			out.print(obj.toString());
+		} catch (Exception e) {
+			System.out.println("로그인 실패");
+						
+		}
+		
+		
+
+		
+	}
 }
